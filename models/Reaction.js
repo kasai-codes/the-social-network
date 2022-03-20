fa269627579999c044750e6fc76bdf4b91230a92
kasai <kasaipreston@gmail.com>
2022-03-20 09:31:10 -0700

    changed date format

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,35 +1,35 @@
-const { Schema, Types } = require('mongoose');
-const dateFormat = require('../utils/dateFormat');
+const { Schema, Types } = require("mongoose");
 
-// reaction schema tied to thought
 const reactionSchema = new Schema(
-    {
-        // set custom id to avoid confusion with parent id
-        reactionId: {
-            type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId()
-        },
-        reactionBody: {
-            type: String,
-            required: true,
-            maxlength: 280
-        },
-        username: {
-            type: String,
-            required: true,
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: createdAtVal => dateFormat(createdAtVal)
-        }
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
     },
-    {
-        toJSON: {
-            getters: true
-        },
-        id: false
-    }
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: function () {
+        return this.toLocaleString();
+      },
+    },
+  },
+
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
 );
 
 module.exports = reactionSchema;
--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,4 @@
 const { Schema, model } = require("mongoose");
-const dateFormat = require("../utils/dateFormat");
 const reactionSchema = require("./Reaction");
 
 // Schema to create Student model
@@ -13,8 +12,10 @@ const thoughtSchema = new Schema({
 
   createdAt: {
     type: Date,
-    default: Date.now(),
-    get: (createdAtVal) => dateFormat(createdAtVal),
+    default: Date.now,
+    get: function () {
+      return this.toLocaleString();
+    },
   },
 
   username: {
