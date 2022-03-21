5ad856390a4f5b5ff110a35a8572fee787d1de20
kasai <kasaipreston@gmail.com>
2022-03-20 19:35:23 -0700

    changed user.js to use poplate. and thoughtcontroller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,106 +1,104 @@
 const { User, Thought } = require("../models");
-// get all users
+
 module.exports = {
-  getUsers(req, res) {
-    User.find()
-      .then((users) => res.json(users))
-      .catch((err) => res.status(500).json(err));
+  // get all users
+  getAllUsers(req, res) {
+      User.find({})
+          .then(dbUserData => res.json(dbUserData))
+          .catch(err => {
+              console.log(err);
+              res.status(400).json(err);
+          });
   },
 
-// get one user by id value
-getSingleUser(req, res) {
-    User.findOne({ _id: req.params.userId })
-      .select('-__v')
-      .then((user) =>
-        !user
-          ? res.status(404).json({ message: 'No user with that ID' })
-          : res.json(user)
-      )
-      .catch((err) => res.status(500).json(err));
+  // get one user by id
+  getUserById({ params }, res) {
+      User.findOne({ _id: params.id })
+          .populate('thoughts')
+          .populate('friends')
+          .select('-__v')
+          .then(dbUserData => {
+              if (!dbUserData) {
+                  res.status(404).json({ message: 'No user found with this id!' });
+                  return;
+              }
+              res.json(dbUserData);
+          })
+          .catch(err => {
+              console.log(err);
+              res.status(400).json(err);
+          });
   },
 
-  // create a new user
-  createUser(req, res) {
-    User.create(req.body)
-      .then((user) => res.json(user))
-      .catch((err) => res.status(500).json(err));
-  },
-
-//   update user
-  updateUser(req, res) {
-    User.findOneAndUpdate(
-      { _id: req.params.thoughtId },
-      { $set: req.body },
-      { runValidators: true, new: true }
-    )
-      .then((thought) =>
-        !thought
-          ? res.status(404).json({ message: 'No thought with this id!' })
-          : res.json(thought)
-      )
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  },
-
-// delete user by id value as well as associated thoughts
-  deleteUser(req, res) {
-    User.findOneAndDelete({ _id: req.params.userId })
-      .then((user) =>
-        !user
-          ? res.status(404).json({ message: 'No user with that ID' })
-          : Application.deleteMany({ _id: { $in: user.thoughts } })
-      )
-      .then(() => res.json({ message: 'User and associated apps deleted!' }))
-      .catch((err) => res.status(500).json(err));
+  // createUser
+  createUser({ body }, res) {
+      User.create(body)
+          .then(dbUserData => res.json(dbUserData))
+          .catch(err => res.status(400).json(err));
   },
 
 
-// add a friend to user friend list
-  addFriend(req, res) {
-    console.log("You are adding an assignment");
-    console.log(req.body);
-    Student.findOneAndUpdate(
-      { _id: req.params.studentId },
-      { $addToSet: { assignments: req.body } },
-      { runValidators: true, new: true }
-    )
-      .then((student) =>
-        !student
-          ? res
-              .status(404)
-              .json({ message: "No student found with that ID :(" })
-          : res.json(student)
-      )
-      .catch((err) => res.status(500).json(err));
+  // update user by id
+  updateUser({ params, body }, res) {
+      User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+          .then(dbUserData => {
+              if (!dbUserData) {
+                  res.status(404).json({ message: 'No user found with this id!' });
+                  return;
+              }
+              res.json(dbUserData);
+          })
+          .catch(err => res.status(400).json(err));
   },
 
+  // delete user
+  deleteUser({ params }, res) {
+      User.findOneAndDelete({ _id: params.id })
+          .then(dbUserData => {
+              if (!dbUserData) {
+                  return res.status(404).json({ message: 'No user found with this id!' });
+              }
 
-// remove friend from user friend list
-  removeFriend(req, res) {
-    Student.findOneAndUpdate(
-      { _id: req.params.friendId },
-      { $pull: { friends: { friendsId: req.params.friendsId } } },
-      { runValidators: true, new: true }
-    )
-      .then((friend) =>
-        !friend
-          ? res
-              .status(404)
-              .json({ message: "No friend found with that ID :(" })
-          : res.json(friend)
-      )
-      .catch((err) => res.status(500).json(err));
+              // bonus: return Thought.deleteMany({ _id: { $in: dbUserData.thoughts } })
+          })
+          .then(() => {
+              res.json({ message: 'user has been deleted.' });
+          })
+          .catch(err => res.status(400).json(err));
   },
 
+  // add friend
+  addFriend({ params }, res) {
+      User.findOneAndUpdate({ _id: params.id }, { $addToSet: { friends: params.friendId } }, { runValidators: true })
+          .then(dbUserData => {
+              if (!dbUserData) {
+                  res.status(404).json({ message: 'No user found with this id!' });
+                  return;
+              }
+              res.json(dbUserData);
+          })
+          .catch(err => res.status(400).json(err));
+  },
 
+  // remove friend
+  removeFriend({ params }, res) {
+      User.findOneAndUpdate({ _id: params.id }, { $pull: { friends: params.friendId } }, { runValidators: true })
+          .then(dbUserData => {
+              if (!dbUserData) {
+                  res.status(404).json({ message: 'No user found with this id!' });
+                  return;
+              }
+              res.json(dbUserData);
+          })
+          .catch(err => res.status(400).json(err));
+  },
 
+}
 
 
   
 
-};
+
 
 
 
--- models/User.js
+++ models/User.js
@@ -41,7 +41,7 @@ const userSchema = new Schema(
 const User = model('User', userSchema);
 
 
-UserSchema.virtual('friendCount').get(function () {
+userSchema.virtual('friendCount').get(function () {
     return this.friends.length;
 });
 
